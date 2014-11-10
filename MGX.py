7ccdbd9c6547947f96624044a4e20faa80911e42
hamerk <kingkyle222@gmail.com>
2014-11-10 19:08:29 +1100

    Update to Master code
    Plus example of output file

--- MGX.py
+++ MGX.py
@@ -41,84 +41,86 @@ while True: # Bact or Fung Run?
 # Sample Information
 
 print "What is the prefix?"
-prefix = raw_input(">>> ")
-print "What is the range of the suffixes?"
-wait(1)
-print "Lowest number?"
-low = int(raw_input(">>> "))
-print "Highest Number?"
-high = int(raw_input(">>> ")) + 1
-print "Any missing numbers?"
-#Move ignore = [] here?
 while True:
-    test = raw_input(">>> ")
-    if bool(test) == False:
+    prefix = raw_input(">>> ")
+    if bool(prefix) == False:
         break
-    elif '-' in test:
-        [test_min, test_max] = test.split('-')
-        for i in range(int(test_min), int(test_max)+1):
-            ignore.append(i)
     else:
-        test = int(test)
-        if test not in range(low, high):
-            print "Try Again."
-            continue
-        else:
-            ignore.append(test)
-    print "Any more missing numbers?"
+        print "What is the range of the suffixes?"
+        wait(1)
+        print "Lowest number?"
+        low = int(raw_input(">>> "))
+        print "Highest Number?"
+        high = int(raw_input(">>> ")) + 1
+        print "Any missing numbers?"
+        ignore = []
+        while True:
+            test = raw_input(">>> ")
+            if bool(test) == False:
+                break
+            elif '-' in test:
+                [test_min, test_max] = test.split('-')
+                for i in range(int(test_min), int(test_max)+1):
+                    ignore.append(i)
+            else:
+                test = int(test)
+                if test not in range(low, high):
+                    print "Try Again."
+                    continue
+                else:
+                    ignore.append(test)
+            print "Any more missing numbers?"
 
-print "Multiple colonies on one plate?"
-wait(1)
-print "Which suffix?"
-mult = {}
-while True:
-    test = raw_input(">>> ")
-    if bool(test) == False:
-        break
-    else:
-        test = int(test)
-        if test not in range(low, high):
-            print "Try Again."
-            continue
-        else:
-            test_num = int(raw_input("How many colonies?\n>>> "))
-            mult[test] = test_num
-    print("Any more multiple colonies?")
+        print "Multiple colonies on one plate?"
+        mult = {}
+        while True:
+            test = raw_input(">>> ")
+            if bool(test) == False:
+                break
+            else:
+                test = int(test)
+                if test not in range(low, high):
+                    print "Try Again."
+                    continue
+                else:
+                    test_num = int(raw_input("How many colonies?\n>>> "))
+                    mult[test] = test_num
+            print("Any more multiple colonies?")
 
-print "Any repeat samples?"
-wait(1)
-print "Which suffix?"
-rep = []
-while True:
-    test = raw_input(">>> ")
-    if bool(test) == False:
-        break
-    else:
-        test = int(test)
-        if test not in range(low, high):
-            print "Try Again."
-            continue
-        else:
-            rep.append(test)
-    print("Any more repeat samples?")
-    
-for i in range(low, high): # Add that prefix to first_list
-    if i in ignore:
-        continue
-    elif i < 10: #Minimum 2 digit suffix
-        if i in mult: #If multiple colonies on plate
-            for let in range(mult[i]): #Add letters
-                first_list.append(prefix + "." + '0' + str(i) + alpha[let])
-        else:
-            first_list.append(prefix + "." + '0' + str(i))
-    else:
-        if i in mult:
-            for let in range(mult[i]):
-                first_list.append(prefix + "." + str(i) + alpha[let])
-        else:
-            first_list.append(prefix + "." + str(i))
-    if i in rep: #If repeat sample
-        first_list[-1] = first_list[-1] + "R" #add 'R'
+        print "Any repeat samples?"
+        rep = []
+        while True:
+            test = raw_input(">>> ")
+            if bool(test) == False:
+                break
+            else:
+                test = int(test)
+                if test not in range(low, high):
+                    print "Try Again."
+                    continue
+                else:
+                    rep.append(test)
+            print("Any more repeat samples?")
+            
+        for i in range(low, high): # Add that prefix to first_list
+            if i in ignore:
+                continue
+            elif i < 10: #Minimum 2 digit suffix
+                if i in mult: #If multiple colonies on plate
+                    for let in range(mult[i]): #Add letters
+                        first_list.append(prefix + "." + '0' + str(i) + alpha[let])
+                else:
+                    first_list.append(prefix + "." + '0' + str(i))
+            else:
+                if i in mult:
+                    for let in range(mult[i]):
+                        first_list.append(prefix + "." + str(i) + alpha[let])
+                else:
+                    first_list.append(prefix + "." + str(i))
+            if i in rep: #If repeat sample
+                first_list[-1] = first_list[-1] + "R" #add 'R'
+
+        print("Is there another prefix?")
 
 #end repeat
 first_list.append(BorF + "NEGW")
@@ -155,5 +157,14 @@ if sec_list[-1] == 'HIDI':
 while len(sec_list)%8 != 0:
     sec_list.append('HIDI')
 
-for last in sec_list:
+for last in sec_list: #Remove in final copy
     print last
+
+record = open(runum + '.txt', 'w')
+count = 0
+for last in sec_list:
+    record.write(last)
+    count += 1
+    if count < len(sec_list):
+        record.write('\n')
+record.close()
